8cc81250e5f051bf48c57df064a036d40988ef8f
sahith00 <bhsahith@yahoo.com>
2018-01-04 19:28:28 -0800

    Intake code updated

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Tests/intakeTest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Tests/intakeTest.java
@@ -1,44 +1,67 @@
 package org.firstinspires.ftc.robotcontroller.internal.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import android.util.Log;
 
+import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 /**
- * Created by sahith on 1/3/18.
+ * Created by vulcanrobotics8375 on 1/4/18.
  */
 
 public class intakeTest extends LinearOpMode {
-    DcMotor rgrab, lgrab;
-    boolean forward = true;
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        rgrab = hardwareMap.dcMotor.get("rgrab");
-        lgrab = hardwareMap.dcMotor.get("lgrab");
-        rgrab.setPower(0.0);
-        lgrab.setPower(0.0);
-
+    DcMotor lintake, rintake;
+    double lintakep, rintakep;
+    public void runOpMode() {
+        lintake = hardwareMap.dcMotor.get("lintake");
+        rintake = hardwareMap.dcMotor.get("rintake");
+        lintakep = -1.0;
+        rintakep = -1.0;
         waitForStart();
         while (opModeIsActive()) {
-            if (gamepad1.a) {
+            if (gamepad1.y) {
                 sleep(250);
-                forward = true;
+                lintakep += .1;
+                if (lintakep > 1.0) {
+                    lintakep = 1.0;
+                }
+            }
+            if (gamepad1.x) {
+                sleep(250);
+                lintakep -= .1;
+                if (lintakep < -1.0) {
+                    lintakep = -1.0;
+                }
             }
             if (gamepad1.b) {
                 sleep(250);
-                forward = false;
+                rintakep += .1;
+                if (rintakep > 1.0) {
+                    rintakep = 1.0;
+                }
             }
-            if (forward) {
-                rgrab.setPower(gamepad1.right_trigger);
-                lgrab.setPower(gamepad1.left_trigger);
+            if (gamepad1.a) {
+                sleep(250);
+                rintakep -= .1;
+                if (rintakep < -1.0) {
+                    rintakep = -1.0;
+                }
             }
-            if (!forward) {
-                rgrab.setPower(-gamepad1.right_trigger);
-                lgrab.setPower(-gamepad1.left_trigger);
+            if (gamepad1.right_bumper) {
+                lintakep = .0;
+                rintakep = .0;
             }
-
-            telemetry.addData("Right power", rgrab.getPower());
-            telemetry.addData("Left power", lgrab.getPower());
+            lintake.setPower(lintakep);
+            rintake.setPower(rintakep);
+            telemetry.addData("Controls: ", "Y increases rintakep, X decreases; B increases lintakep, A decreases; Right bumper sets both to 0");
+            telemetry.addData("lintakep: ", lintake.getPower());
+            telemetry.addData("rintakep: ", rintake.getPower());
             telemetry.update();
         }
     }
