fe6f7faa147e3c8f8b570e5becff42bc2add1463
sahith00 <bhsahith@yahoo.com>
2018-01-17 20:57:03 -0800

    Update for lift pid

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Cisco/ciscoTeleOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Cisco/ciscoTeleOp.java
@@ -1,30 +1,45 @@
 package org.firstinspires.ftc.robotcontroller.internal.Cisco;
+import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by sahith on 12/10/17.
  */
-
 public class ciscoTeleOp extends LinearOpMode{
-
     DcMotor frdrive, fldrive, brdrive, bldrive;
     DcMotor rgrab, lgrab;
     DcMotor lift;
-    Servo flipper;
-    //Servo cat, knock;
-
+    //    Servo cat, knock;
+    Servo rflip, lflip;
     final static double CAT_STOW = 0.66;
     final static double KNOCK_CENTER = 0.38;
+    final static double RFLIP_DEPOSIT = 0.195; //0.47
+    final static double RFLIP_ZERO = 0.485; //0.74
+    final static double RFLIP_GRAB = 0.555;
+    final static double LFLIP_DEPOSIT = 0.8795; //0.53
+    final static double LFLIP_ZERO = 0.3495; //0.26
+    final static double LFLIP_GRAB = 0.2395;
+    final static double LEVEL_ONE = 0;
+    final static double LEVEL_TWO = 372;
+    final static double LEVEL_THREE = 770;
+    double intakep;
+    boolean lift_zero;
 
-    int direction = 1;
+    double p_turn = .03;//0.008;
+    double i_turn = .00; //.0045; //.003;
+    double d_turn = .002; //.04 //.0045;
+    double f_turn = .0;
+    double pT = 0;
+    double pE = 0;
+    double tE = 0;
 
-    boolean stopped;
+    ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -35,135 +50,125 @@ public class ciscoTeleOp extends LinearOpMode{
         lift = hardwareMap.dcMotor.get("lift");
         rgrab = hardwareMap.dcMotor.get("rintake");
         lgrab = hardwareMap.dcMotor.get("lintake");
-        flipper = hardwareMap.servo.get("flipper");
-        //cat = hardwareMap.servo.get("cat");
-        //knock = hardwareMap.servo.get("knock");
-
+//        cat = hardwareMap.servo.get("cat");
+//        knock = hardwareMap.servo.get("knock");
+        rflip = hardwareMap.servo.get("rflip");
+        lflip = hardwareMap.servo.get("lflip");
         fldrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frdrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fldrive.setDirection(DcMotorSimple.Direction.REVERSE);
         frdrive.setDirection(DcMotorSimple.Direction.REVERSE);
         bldrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         brdrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); //so that lift can hold its position
+        zero(); //inits flipper parallel to the ground
         fldrive.setPower(0);
         frdrive.setPower(0);
         bldrive.setPower(0);
         brdrive.setPower(0);
-        //lift.setPower(0);
+        lift.setPower(0);
         rgrab.setPower(0);
         lgrab.setPower(0);
-        //cat.setPosition(CAT_STOW);
-        //knock.setPosition(KNOCK_CENTER);
-
+/*        cat.setPosition(CAT_STOW);
+        knock.setPosition(KNOCK_CENTER);*/
         double multiplier = 1.0;
+        intakep = .0;
+        lift_zero = true;
 
         waitForStart();
-
         while(opModeIsActive()) {
-            //cat.setPosition(CAT_STOW);
-            //knock.setPosition(KNOCK_CENTER);
-
-            //-----------------------------------------------------------------------------
+          /*  cat.setPosition(CAT_STOW);
+            knock.setPosition(KNOCK_CENTER);*/
             // DRIVE ROBOT
-            if (gamepad1.x) {
-                multiplier = 0.5;
+            if (gamepad1.dpad_left || gamepad1.dpad_right || gamepad1.dpad_up || gamepad1.dpad_down) {
+                if (gamepad1.dpad_up) {
+                    fldrive.setPower(.8);
+                    frdrive.setPower(.8);
+                    bldrive.setPower(.8);
+                    brdrive.setPower(.8);
+                }
+                if (gamepad1.dpad_right) {
+                    fldrive.setPower(.8);
+                    frdrive.setPower(-.8);
+                    bldrive.setPower(-.8);
+                    brdrive.setPower(.8);
+                }
+                if (gamepad1.dpad_left) {
+                    fldrive.setPower(-.8);
+                    frdrive.setPower(.8);
+                    bldrive.setPower(.8);
+                    brdrive.setPower(-.8);
+                }
+                if (gamepad1.dpad_down) {
+                    fldrive.setPower(-.8);
+                    frdrive.setPower(-.8);
+                    bldrive.setPower(-.8);
+                    brdrive.setPower(-.8);
+                }
+            } else {
+                mecanum(fldrive, frdrive, bldrive, brdrive, multiplier);
             }
-            if (gamepad1.y) {
-                multiplier = 0.3;
-            }
-            if (gamepad1.a) {
-                multiplier = 0.8;
-            }
-
-            if(gamepad1.dpad_down) {
-                fldrive.setPower(-0.8);
-                frdrive.setPower(-0.8);
-                bldrive.setPower(-0.8);
-                brdrive.setPower(-0.8);
-            }
-            if(gamepad1.dpad_up) {
-                fldrive.setPower(0.8);
-                frdrive.setPower(0.8);
-                bldrive.setPower(0.8);
-                brdrive.setPower(0.8);
-            }
-            if(gamepad1.dpad_left) {
-                fldrive.setPower(0.8);
-                frdrive.setPower(-0.8);
-                bldrive.setPower(-0.8);
-                brdrive.setPower(0.8);
-            }
-            if(gamepad1.dpad_right) {
-                fldrive.setPower(-0.8);
-                frdrive.setPower(0.8);
-                bldrive.setPower(0.8);
-                brdrive.setPower(-0.8);
-            }
-
-            mecanum(fldrive, frdrive, bldrive, brdrive, multiplier);
             //-----------------------------------------------------------------------------
-
-
             //-----------------------------------------------------------------------------
             // GRAB RELIC AND DEPOSIT
-
-            //-----------------------------------------------------------------------------
-
-
             //-----------------------------------------------------------------------------
             // GRAB GLYPH AND DEPOSIT
-
-            if (gamepad2.a) {
-                rgrab.setPower(-0.32);
-                lgrab.setPower(-0.8);
-                stopped = false;
+            if (gamepad2.right_bumper) {
+                intakep = 1.0;
             }
-            if (gamepad2.b) {
-                rgrab.setPower(-0.7);
-                lgrab.setPower(-0.7);
-                stopped = false;
+            if (gamepad2.left_bumper) {
+                intakep = .0;
+            }
+            if (gamepad1.y) {
+                sleep(250);
+                intakep += 0.1;
+            }
+            if (gamepad1.a) {
+                sleep(250);
+                intakep -= 0.1;
+            }
+            if (intakep == .0) {// we can only use triggers to fix cubes when the intake is not running
+                rgrab.setPower(-.5*Math.signum(gamepad2.right_trigger));
+                lgrab.setPower(-.5*Math.signum(gamepad2.left_trigger));
+            } else {
+                rgrab.setPower(Range.clip(intakep, -1, 1));
+                lgrab.setPower(Range.clip(intakep, -1, 1));
+            }
+
+            if (lift_zero) {
+                lift.setPower(Range.clip(gamepad2.right_stick_y, -.6, .5));
+            }
+
+            if (gamepad2.y) {
+                moveLevels(LEVEL_THREE, lift_zero);
             }
             if (gamepad2.x) {
-                rgrab.setPower(0.0);
-                rgrab.setPower(0.0);
-                stopped = true;
+                moveLevels(LEVEL_TWO, lift_zero);
             }
-            if (gamepad2.y) {
-                sleep(250);
-                direction = -direction;
+            if (gamepad2.a) {
+                moveLevels(LEVEL_ONE, lift_zero);
             }
 
-            if (stopped) {
-                rgrab.setPower(-direction * 0.7 * Math.signum(gamepad2.right_trigger));
-                lgrab.setPower(-direction * 0.7 * Math.signum(gamepad2.left_trigger));
+            if (gamepad2.dpad_left) { //zero position, when flipper is parallel to ground
+                zero();
             }
-            lift.setPower(gamepad2.right_stick_y);
-            if (gamepad2.dpad_up) {
-                flipper.setPosition(1.0);
-                sleep(500);
-                flipper.setPosition(0.0);
+            if (gamepad2.dpad_down) { //grab position, when we're picking up cubes
+                grab();
             }
-            if (gamepad2.dpad_right) {
-                sleep(250);
-                flipper.setPosition(1.0);
+            if (gamepad2.dpad_up) { //deposit position, when we're depositing cubes
+                deposit();
             }
-            if (gamepad2.dpad_left) {
-                sleep(250);
-                flipper.setPosition(0.0);
-            }
-            //-----------------------------------------------------------------------------
-
-
             //-----------------------------------------------------------------------------
             // TELEMETRY
-            telemetry.addData("multiplier", multiplier);
+            telemetry.addData("Lift Encoder Count: ", lift.getCurrentPosition());
+            telemetry.addData("Lift Power", lift.getPower());
+            telemetry.addData("Intake power", intakep);
             telemetry.update();
             //-----------------------------------------------------------------------------
         }
     }
-
     public void mecanum(DcMotor fl, DcMotor fr, DcMotor bl, DcMotor br, double multiplier) {
         double joyly = gamepad1.left_stick_y;
         double joylx = -(gamepad1.left_stick_x);
@@ -172,32 +177,83 @@ public class ciscoTeleOp extends LinearOpMode{
         double theta = Math.atan2(joyly, joylx);
         double v0 = joyrx;
         //v0 = bumpers;
-        double v1 = vd*Math.sin(theta+(Math.PI/4))-v0; //fl
-        double v2 = vd*Math.cos(theta + (Math.PI / 4))-v0; //fr
-        double v3 = vd*Math.cos(theta+(Math.PI/4))+v0; //bl
-        double v4 = vd*Math.sin(theta + (Math.PI / 4))+v0; //br
+        double v1 = vd*Math.sin(theta+(Math.PI/4))+v0; //fl
+        double v2 = vd*Math.cos(theta + (Math.PI / 4))+v0; //fr
+        double v3 = vd*Math.cos(theta+(Math.PI/4))-v0; //bl
+        double v4 = vd*Math.sin(theta + (Math.PI / 4))-v0; //br
         double temp_max = Math.max(Math.abs(v1), Math.abs(v2));
         double temp_max2 = Math.max(temp_max, Math.abs(v3));
         double max = Math.max(temp_max2, Math.abs(v4));
-
-        if (max > 1.0) {
+        if (max > 1) {
             fl.setPower(multiplier * (v1/max));
             fr.setPower(multiplier * (v2/max));
             bl.setPower(multiplier * (v3/max));
             br.setPower(multiplier * (v4/max));
-        }
-        else if (max != 0) {
-            fl.setPower(multiplier * (v1));
-            fr.setPower(multiplier * (v2));
-            bl.setPower(multiplier * (v3));
-            br.setPower(multiplier * (v4));
-        }
-        else {
-            fl.setPower(0.0);
-            fr.setPower(0.0);
-            bl.setPower(0.0);
-            br.setPower(0.0);
+        } else {
+            fl.setPower(v1);
+            fr.setPower(v2);
+            bl.setPower(v3);
+            br.setPower(v4);
         }
     }
+    public void grab() {
+        rflip.setPosition(RFLIP_GRAB);
+        lflip.setPosition(LFLIP_GRAB);
+    }
+    public void zero() {
+        rflip.setPosition(RFLIP_ZERO);
+        lflip.setPosition(LFLIP_ZERO);
+    }
+    public void deposit() {
+        rflip.setPosition(RFLIP_DEPOSIT);
+        lflip.setPosition(LFLIP_DEPOSIT);
+    }
 
+    public void movePID(double distance, double margin) {
+        startDegreeController();
+        double pYaw = lift.getCurrentPosition();
+        while (Math.abs(lift.getCurrentPosition() - distance) > margin || Math.abs(pYaw - lift.getCurrentPosition()) > .05) {
+            double change = tickController(distance);
+            lift.setPower(Range.clip(change, -1, 1));
+            pYaw = lift.getCurrentPosition();
+            telemetry.addData("In While loop", lift.getCurrentPosition());
+            if (gamepad2.back) {
+                break;
+            }
+        }
+        lift.setPower(0.0);
+    }
+
+    public void startDegreeController(){
+        pT = runtime.time();
+        pE = 0;
+        tE = 0;
+    }
+
+    public double tickController(double ticks){
+        double ans = 0;
+        double e = -(lift.getCurrentPosition() - ticks);
+        double dE = e-pE;
+        double dT = runtime.time() - pT;
+        Log.i("PID turn", "e: " + e);
+        Log.i("PID Turn", "i: " + i_turn * tE);
+        Log.i("PID Turn", "d: " + d_turn * dE / dT);
+        Log.i("PID Turn", "p: " + p_turn * e);
+        ans = p_turn*e+ i_turn*tE + d_turn*dE/dT;// +f_turn* Math.signum(e);
+        pT = runtime.time();
+        pE = e;
+        tE += e*dT;
+//        if (Math.signum(e) != Math.signum(pE)){
+//            tE = 0;
+//        }
+        tE = Range.clip(tE * i_turn, -.15, 0.15);///i_turn;
+        ans = Range.clip(ans, 0, .7);
+        return ans;
+    }
+
+    public void moveLevels(double level, boolean lift_zero) {
+        lift_zero = false;
+        movePID(level, 30);
+        lift_zero = true;
+    }
 }
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Tests/servoTest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Tests/servoTest.java
@@ -6,13 +6,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 /**
  * Created by sahith on 10/12/17.
  */
-public class servoTest extends LinearOpMode{
-    Servo servo1;
+public class servoTest extends LinearOpMode {
+    Servo servo1, servo2;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        servo1 = hardwareMap.servo.get("flipper");
+        servo1 = hardwareMap.servo.get("rflip");
+        servo2 = hardwareMap.servo.get("lflip");
         servo1.setPosition(0.5);
+        servo2.setPosition(0.5);
         double lchange = 0.01, rchange = 0.01;
         double cchange[] = new double[2];
         cchange[0] = 0.01;
@@ -21,7 +23,7 @@ public class servoTest extends LinearOpMode{
 
         waitForStart();
 
-        while(opModeIsActive()) {
+        while (opModeIsActive()) {
             if (gamepad1.x) {
                 sleep(250);
                 lchange += cchange[count];
@@ -30,7 +32,7 @@ public class servoTest extends LinearOpMode{
                 }
             }
 
-            if(gamepad1.y) {
+            if (gamepad1.y) {
                 sleep(250);
                 rchange += cchange[count];
                 if (rchange >= 1.0) {
@@ -38,10 +40,10 @@ public class servoTest extends LinearOpMode{
                 }
             }
 
-            if(gamepad1.right_bumper) {
+            if (gamepad1.right_bumper) {
                 sleep(500);
                 count++;
-                if (count==2) {
+                if (count == 2) {
                     count = 0;
                 }
             }
@@ -57,16 +59,34 @@ public class servoTest extends LinearOpMode{
             if (gamepad1.dpad_up) {
                 sleep(500);
                 double ltemp = servo1.getPosition() - lchange;
-                if (ltemp <= 0.0) {
+                if (ltemp < 0.0) {
                     ltemp = 0.0;
                 }
                 servo1.setPosition(ltemp);
+            }
+
+            if (gamepad1.a) {
+                sleep(500);
+                double rtemp = servo2.getPosition() - rchange;
+                if (rtemp <= 0.0) {
+                    rtemp = 0.0;
+                }
+                servo2.setPosition(rtemp);
+            }
+            if (gamepad1.b) {
+                sleep(500);
+                double rtemp = servo2.getPosition() + rchange;
+                if (rtemp >= 1.0) {
+                    rtemp = 1.0;
+                }
+                servo2.setPosition(rtemp);
             }
 
             telemetry.addData("cchange", cchange[count]);
             telemetry.addData("lchange", lchange);
             telemetry.addData("rchange", rchange);
             telemetry.addData("servo1", servo1.getPosition());
+            telemetry.addData("servo2", servo2.getPosition());
             telemetry.update();
         }
     }
